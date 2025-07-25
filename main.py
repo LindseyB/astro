59d1c81e298bf2afbdc0ae12c098aafd5f9a9d55
Lindsey Bieda <hazardous.narayan@gmail.com>
2025-07-24 21:39:09 -0700

    Add more about houses etc

--- main.py
+++ main.py
@@ -19,7 +19,23 @@ sun = chart.get('Sun')
 moon = chart.get('Moon')
 ascendant = chart.get('Asc')
 
+house2 = chart.get('House2')  # Example of getting a house, if needed
+
+
 # ---- Output Results ----
 print (f"☀️ {sun.sign}")
 print (f"🌙 {moon.sign}")
 print (f"⬆️ {ascendant.sign}")
+print (f"🏠 {house2.sign}")
+
+# ---- List Planets in Houses ----
+for house in chart.houses:
+    chart.objects.getObjectsInHouse(house)
+    planets = ""
+    for obj in chart.objects.getObjectsInHouse(house):
+        # I tried (isPlanet, and it counts things like Fortuna, which I don't want)
+        if obj.id in ['Sun', 'Moon', 'Mercury', 'Venus', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto']:
+            planets += f"{obj.id} "
+    if planets:
+        print(f"{house.id}: {house.sign}, {planets.strip()}")
+
